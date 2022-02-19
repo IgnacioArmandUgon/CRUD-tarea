d1866207236ded8c7b55031a35b848ca2128d91c
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-02-19 17:15:42 -0300

    Proyecto base creado

---
+++ src/test/java/com/CRUDtarea/CrudTareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRUDtarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
